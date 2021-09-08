749c125a3b5902cc21f945e525c22ee000b0b24a
k18 <510674127@qq.com>
2021-09-08 17:09:58 +0800

    添加l.md,read.md,test.py文件

---
+++ l.md
@@ -0,0 +1,11 @@
+git  mv  重命名文件
+
+不要直接mv 文件，这样git会认为是删除了一个文件，再新建了一个文件
+需要执行git rm   oldwenjain。   git add newwenjian
+
+
+git  log  --online  简洁的查看
+git log -n2 --oneline。查看最近2次的信息
+
+git  branch -v  查看分支的信息
+
---
+++ week01/test.py
@@ -0,0 +1,19 @@
+#/url/bin/env python 
+#coding=utf-8
+class ClassA():
+    var1 = 100
+    var2 = 90
+    var3 = 80
+    
+    def __init__(self):
+        print("实例化成功")
+
+    def fun1():
+        print("我是fun1")
+    
+    def fun2():
+        print("我是fun2")
+    
+    def fun3():
+        print("我是fun3")
+kk=ClassA()
